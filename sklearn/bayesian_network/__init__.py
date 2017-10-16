74a8427ed9988e618d8dc879e4390219cde68f41
Oliver McClellan <om639@york.ac.uk>
2017-10-16 23:57:44 +0100

    Initial implementation of discrete data loader.

---
+++ sklearn/bayesian_network/__init__.py
@@ -0,0 +1,3 @@
+"""
+The :mod:`sklearn.bayesian_network` module implements Bayesian network structure learning.
+"""
---
+++ sklearn/bayesian_network/graph.py
@@ -0,0 +1,17 @@
+"""
+Directed acyclic graph (DAG) representation for Bayesian networks.
+"""
+
+
+class Graph(object):
+    """
+    Represents a directed acyclic graph (DAG).
+    """
+    pass
+
+
+class Vertex(object):
+    """
+    Represents a vertex in a DAG.
+    """
+    pass
