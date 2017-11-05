6d531af515ba8da0269fbabb1f98c31a4032918c
Oliver McClellan <om639@york.ac.uk>
2017-11-05 23:01:14 +0000

    Add sklearn-style comments to BN, RV and scoring functions.

--- sklearn/bayesian_network/bn.py
+++ sklearn/bayesian_network/bn.py
@@ -1,28 +1,51 @@
-class BN(object):
-    """
-    Represents a Bayesian network.
-    """
+"""
+Representations of Bayesian networks and random variables.
+"""
 
-    # TODO: make comments sklearn style
+
+class BN(object):
+    """Represents a Bayesian network.
+
+    Attributes
+    ----------
+    rvs : dict of string to ``RV``
+        Maps the name of a random variable names to the corresponding ``RV``.
+
+    parents: dict of string to list of ``RV``
+        Maps the name of a random variable to a list of its parent ``RV``s.
+    """
 
     def __init__(self):
-        """
-        Initialize a new BN.
-        """
         self.rvs = dict()
         self.parents = dict()
 
     def add_rv(self, rv):
-        """
-        Add an RV to the network.
+        """Add a random variable to the Bayesian network.
+
+        Sets ``rv.bn`` to be equal to this ``BN``.
+
+        Parameters
+        ----------
+        rv : ``RV``
+            The random variable to be added to the network. The ``rv.name``
+            attribute must be unique within the network, otherwise this method
+            will replace the existing RV with the same name with the new one.
         """
         self.rvs[rv.name] = rv
         self.parents[rv.name] = []
         rv.bn = self
 
     def remove_rv(self, rv):
-        """
-        Remove an RV from the network.
+        """Remove a random variable from the network.
+
+        If ``rv`` is a parent to any other RVs, it is automatically removed
+        from their respective parent lists.
+
+        Parameters
+        ----------
+        rv : ``RV``
+            The random variable to be removed from the network. If ``rv`` is
+            not part of the network, this method does nothing.
         """
         del self.rvs[rv.name]
         del self.parents[rv.name]
@@ -35,48 +58,116 @@ class BN(object):
                 pass
 
     def add_edge(self, a, b):
-        """
-        Add an edge from ``a`` to ``b``.
+        """Add an edge from RV ``a`` to RV ``b``.
+
+        If there is already an edge from ``a`` to ``b``, this method returns
+        ``false``.
+
+        If the new edge would cause a cycle, it is not added and this method
+        returns ``false``.
+
+        Parameters
+        ----------
+        a : ``RV``
+            The random variable from which the new edge should be added.
+
+        b : ``RV``
+            The random variable to which the new edge should be added.
+
+        Returns
+        -------
+        ``true`` if the new edge was successfully added, ``false`` otherwise.
         """
         # TODO: ensure new edge does not make cycle
         if not a == b and not self.has_edge(a, b):
             self.parents[b.name].append(a)
+            return True
+        return False
 
     def has_edge(self, a, b):
-        """
-        Return ``True`` if there is an edge from ``a`` to ``b``, and ``False`` otherwise.
+        """Return whether or not there exists an edge from RV ``a`` to RV ``b``
+        in the network.
+
+        Parameters
+        ----------
+        a : ``RV``
+            The random variable from which to check if an edge exists.
+
+        b : ``RV``
+            The random variable to which to check if an edge exists.
+
+        Returns
+        -------
+        ``true`` if there is an edge from ``a`` to ``b``, false otherwise.
         """
         return a in self.parents[b.name]
 
     def remove_edge(self, a, b):
-        """
-        Remove an edge from ``a`` to ``b``.
+        """Remove the edge from RV ``a`` to RV ``b``.
+
+        If there is no edge from ``a`` to ``b``, this method does nothing.
+
+        Parameters
+        ----------
+        a : ``RV``
+            The random variable from which the edge to be removed originates.
+
+        b : ``RV``
+            The random variable to which the edge to be removed points.
         """
         self.parents[b.name].remove(a)
 
 
 class RV(object):
-    """
-    Represents a random variable in a BN.
+    """Represents a random variable in a Bayesian network.
+
+    Parameters
+    ----------
+    name : string
+        The name of the random variable. Must be unique within a network.
+
+    values : list of string
+        The list of possible values that the random variable can take.
+
+    Attributes
+    ----------
+    name : string
+        The name of the random variable.
+
+    values : list of string
+        The list of possible values that the random variable can take.
+
+    bn : ``BN``
+        The ``BN`` that this ``RV`` is a part of.
     """
 
     def __init__(self, name, values):
-        """
-        Initialize a new RV with the specified name and possible values.
-        """
         self.name = name
         self.values = values
         self.bn = None
 
     @property
     def parents(self):
+        """Return the parent RVs of this ``RV``.
+
+        If this ``RV`` is not part of a ``BN``, a ``ValueError`` is raised.
+
+        Returns
+        -------
+        parents : list of ``RV``
+            The parent RVs of this ``RV``.
         """
-        Return the parent RVs of this RV.
-        """
-        return self.bn.parents[self.name]
+        try:
+            return self.bn.parents[self.name]
+        except AttributeError:
+            raise ValueError('RV not part of BN')
 
     def __repr__(self):
-        """
-        Return a string representation of this RV.
+        """Return a string representation of this ``RV``.
+
+        Returns
+        -------
+        repr : string
+            A string representation of this ``RV``.
         """
         return "RV(name='{}', values={!r})".format(self.name, self.values)
--- sklearn/bayesian_network/score.py
+++ sklearn/bayesian_network/score.py
@@ -1,26 +1,34 @@
+"""
+Scoring functions for Bayesian networks.
+"""
 import math
 import numpy as np
 
 from collections import Counter, defaultdict
 
 
-def bic(bn, data):
-    return log_likelihood(bn, data) - 0.5 * math.log(len(data)) * _dim(bn)
-
-
-def _dim(bn):
-    return sum(_dim_rv(rv) for rv in bn.rvs.values())
-
-
-def _dim_rv(rv):
-    return np.prod([len(parent.values) for parent in rv.parents]) * (len(rv.values) - 1)
-
-
 def log_likelihood(bn, data):
+    """Computer the log-likelihood for the given network with respect to the
+    given data.
+
+    Parameters
+    ----------
+    bn : ``BN``
+        The network to compute the log-likelihood for.
+
+    data : ``numpy.recarray``
+        The data to use when computing the log-likelihood.
+
+    Returns
+    -------
+    ll : float
+        The log-likelihood of ``bn`` given ``data``.
+    """
     return sum(_log_likelihood_rv(rv, data) for rv in bn.rvs.values())
 
 
 def _log_likelihood_rv(rv, data):
+    """Return the log-likelihood for a single ``RV``."""
     ll = 0
     for count_key, counts in _count_rv(rv, data).items():
         parent_count = sum(counts.values())
@@ -30,8 +38,39 @@ def _log_likelihood_rv(rv, data):
 
 
 def _count_rv(rv, data):
+    """Return the parent configuration counts for a single ``RV``."""
     counts = defaultdict(Counter)
     for row in data:
         count_key = tuple(row[parent.name] for parent in rv.parents)
         counts[count_key][row[rv.name]] += 1
     return counts
+
+
+def bic(bn, data):
+    """Compute the Bayesian Information Criterion (BIC) score for the given
+    network with respect to the given data.
+
+    Parameters
+    ----------
+    bn : ``BN``
+        The network to compute the score for.
+
+    data : ``numpy.recarray``
+        The data to use when computing the score.
+
+    Returns
+    -------
+    bic : float
+        The BIC score for ``bn`` given ``data``.
+    """
+    return log_likelihood(bn, data) - 0.5 * math.log(len(data)) * _dim(bn)
+
+
+def _dim(bn):
+    """Return the dimension for the given ``BN``."""
+    return sum(_dim_rv(rv) for rv in bn.rvs.values())
+
+
+def _dim_rv(rv):
+    """Return the dimension for the given ``RV``."""
+    return np.prod([len(parent.values) for parent in rv.parents]) * (len(rv.values) - 1)
